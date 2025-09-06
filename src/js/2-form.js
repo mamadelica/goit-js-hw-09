26bb51e9149bdf68e7706666c9e20f22708d1ccf
Anastasia Mamatova <mamatovaanastasia31@gmail.com>
2025-09-06 15:11:45 +0300

    js hw 9

---
+++ src/js/2-form.js
@@ -0,0 +1,55 @@
+const formData = {
+  email: '',
+  message: '',
+};
+
+const form = document.querySelector('.feedback-form');
+console.log(form);
+
+form.addEventListener('submit', handleSubmit);
+
+form.addEventListener('input', event => {
+  formData[event.target.name] = event.target.value;
+  toLocalStorage();
+});
+
+function toLocalStorage() {
+  localStorage.setItem('feedback-form-state', JSON.stringify(formData));
+}
+
+function restoreFromData() {
+  const savedData = localStorage.getItem('feedback-form-state');
+  let parsedData = null;
+
+  if (!savedData) return;
+
+  try {
+    parsedData = JSON.parse(savedData);
+  } catch (error) {
+    console.error('error', error);
+  }
+
+  if (parsedData) {
+    form.elements.email.value = parsedData.email || '';
+    formData.email = parsedData.email || '';
+    form.elements.message.value = parsedData.message || '';
+    formData.message = parsedData.message || '';
+  }
+}
+
+function handleSubmit(event) {
+  event.preventDefault();
+  if (formData.email === '' || formData.message === '') {
+    alert('Fill please all fields');
+    return;
+  }
+  console.log(formData);
+  localStorage.removeItem('feedback-form-state');
+  formData.email = '';
+  formData.message = '';
+  form.reset();
+}
+
+document.addEventListener('DOMContentLoaded', restoreFromData);
+
+console.log(formData);
